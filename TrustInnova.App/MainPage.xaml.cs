20411d28370af7f3a4521158d930a559658719b7
xcssa <imxcssa@qq.com>
2024-11-08 04:21:41 +0800

    mobile support

--- TrustInnova.App/MainPage.xaml.cs
+++ TrustInnova.App/MainPage.xaml.cs
@@ -4,7 +4,7 @@
     {
         public MainPage()
         {
-            if (OperatingSystem.IsIOS())
+            if (OperatingSystem.IsIOS() && !OperatingSystem.IsMacCatalyst() && !OperatingSystem.IsMacOS())
                 this.Padding = new Thickness(0, 44, 0, 0);
             InitializeComponent();
         }
